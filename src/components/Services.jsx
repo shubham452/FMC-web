ee91e65e3285223d596bbd1d76d181ebb3befed4
shubham452 <shubham.sk70@gmail.com>
2025-11-19 21:51:42 +0530

    changing file

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import Dash from '../assets/Dash.png';
 import LMS from '../assets/LMS.jfif';
 
-// --- Icons for your 5 services ---
+
 const InspectionIcon = () => (
   <svg className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
     <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
