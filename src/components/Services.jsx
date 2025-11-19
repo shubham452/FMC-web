af2ad5001083440e4f20f28f792e9d1443313166
shubham452 <shubham.sk70@gmail.com>
2025-11-19 22:22:32 +0530

    message

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import Dash from '../assets/Dash.png';
 import LMS from '../assets/LMS.jfif';
+import Dash from '../assets/Dash.png';
+
 
 
 const InspectionIcon = () => (
